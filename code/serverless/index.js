bb7c43e550297e08b71ab7623af9a2aca250bfec
azat-co <hi@azat.co>
2017-04-26 16:38:12 -0700

    add changes :customs: :earth_africa:

--- code/serverless/index.js
+++ code/serverless/index.js
@@ -1,47 +1,42 @@
-'use strict';
+'use strict'
 
-console.log('Loading function');
+console.log('Loading function')
+const doc = require('dynamodb-doc')
+const dynamo = new doc.DynamoDB()
 
-const doc = require('dynamodb-doc');
-
-const dynamo = new doc.DynamoDB();
-
-
-/**
- * Demonstrates a simple HTTP endpoint using API Gateway. You have full
- * access to the request and response payload, including headers and
- * status code.
- *
- * To scan a DynamoDB table, make a GET request with the TableName as a
- * query string parameter. To put, update, or delete an item, make a POST,
- * PUT, or DELETE request respectively, passing in the payload to the
- * DynamoDB API as a JSON body.
- */
+// All the request info in event
+// "handler" is defined on the function creation
 exports.handler = (event, context, callback) => {
 
+    // Callback to finish response
     const done = (err, res) => callback(null, {
         statusCode: err ? '400' : '200',
         body: err ? err.message : JSON.stringify(res),
         headers: {
             'Content-Type': 'application/json',
         }
-    });
+    })
+    // To support mock testing, accept object not just strings
     if (typeof event.body == 'string')
         event.body = JSON.parse(event.body)
     switch (event.httpMethod) {
+        // Table name and key are in payload
         case 'DELETE':
-            dynamo.deleteItem(event.body, done);
-            break;
+            dynamo.deleteItem(event.body, done)
+            break
+        // No payload, just a query string param
         case 'GET':
-            dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
-            break;
+            dynamo.scan({ TableName: event.queryStringParameters.TableName }, done)
+            break
+        // Table name and key are in payload
         case 'POST':
-            dynamo.putItem(event.body, done);
-            break;
+            dynamo.putItem(event.body, done)
+            break
+        // Table name and key are in payload
         case 'PUT':
-            dynamo.updateItem(event.body, done);
-            break;
+            dynamo.updateItem(event.body, done)
+            break
         default:
-            done(new Error(`Unsupported method "${event.httpMethod}"`));
+            done(new Error(`Unsupported method "${event.httpMethod}"`))
     }
-};
+}
